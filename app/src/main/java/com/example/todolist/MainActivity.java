645774cc376593b153740978c502fc248077776b
theNAFIZ <nrpustcse@gmail.com>
2022-01-16 23:03:24 +0600

    Added file read write functionalities from file.

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -5,9 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,21 +34,84 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listItems = new ArrayList<>();
+        try {
+            readFromFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         input = (EditText) findViewById(R.id.inputbox);
         btn = (Button) findViewById(R.id.saveBtn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String msg = input.getText().toString();
+                if (!msg.equals("")) {
+                    writeToFile(msg);
+                }
+                try {
+                    readFromFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         rcv = (RecyclerView) findViewById(R.id.rcvID);
 //        rcv.setHasFixedSize(true);
         rcv.setLayoutManager(new LinearLayoutManager(this));
 
-        listItems = new ArrayList<>();
+        //listItems.add(new ListItem("First Todo!"));
 
-        listItems.add(new ListItem("First Todo!"));
-
-        //TODO: File read write functionalities
-        //TODO: Edit layout and fix overlapping issues
+        //TODO: Database access. Edit read write method and implement appropriately.
 
         rcv.setAdapter(new MyAdapter(this, listItems));
 
+    }
+
+    private void writeToFile(String msg) {
+
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("todolist.txt", MODE_APPEND));
+
+            outputStreamWriter.write(msg);
+            outputStreamWriter.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private String readFromFile() throws IOException {
+
+        String result = "";
+
+        InputStream inputStream = openFileInput("todolist.txt");
+
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String tempString = "";
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while( (tempString = bufferedReader.readLine()) != null) {
+
+                listItems.add(new ListItem(tempString));
+                stringBuilder.append(tempString);
+
+            }
+
+            inputStream.close();
+            result = stringBuilder.toString();
+
+        }
+
+        return result;
     }
 }
